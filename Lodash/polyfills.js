ea800d132fbe64c162f820a3c4c8e7876f96e61d
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2025-03-25 13:02:14 +0530

    throttle

--- Lodash/polyfills.js
+++ Lodash/polyfills.js
@@ -9,3 +9,26 @@ const debounce = (func, delay) => {
     timerId = setTimeout(() => func.apply(context, args), delay);
   };
 };
+
+const throttle = (func, limit) => {
+  let timerId;
+  let lastRan;
+
+  return function () {
+    const context = this;
+    const args = arguments;
+
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(timerId);
+      timerId = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
