a79602d24bc831e315eb109d47bcafe90d818b4e
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2025-03-25 13:15:38 +0530

    memoize

--- Lodash/polyfills.js
+++ Lodash/polyfills.js
@@ -32,3 +32,19 @@ const throttle = (func, limit) => {
     }
   };
 };
+
+const memoize = function (fn) {
+  const cache = {};
+
+  return function () {
+    const KEY = JSON.stringify(arguments);
+
+    if (cache[KEY]) {
+      return cache[KEY];
+    }
+
+    const evaluatedValue = fn(...arguments);
+    cache[KEY] = evaluatedValue;
+    return evaluatedValue;
+  };
+};
