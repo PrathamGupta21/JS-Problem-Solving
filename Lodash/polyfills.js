ca88d10c789120417950377f6f206eb878f5829b
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2025-03-25 12:57:22 +0530

    debouce

---
+++ Lodash/polyfills.js
@@ -0,0 +1,11 @@
+const debounce = (func, delay) => {
+  let timerId;
+
+  return function () {
+    const context = this;
+    const args = arguments;
+
+    clearTimeout(timerId);
+    timerId = setTimeout(() => func.apply(context, args), delay);
+  };
+};
